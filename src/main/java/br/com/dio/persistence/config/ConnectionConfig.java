2f7bb3d6eafc56a069fa7e263567f3a4f0d8eada
Tiago Cruz <107926934+GuinhoUrahara@users.noreply.github.com>
2025-03-25 17:25:14 -0300

    Correções no projeto: Lombok, conexão com banco e Gradle

--- src/main/java/br/com/dio/persistence/config/ConnectionConfig.java
+++ src/main/java/br/com/dio/persistence/config/ConnectionConfig.java
@@ -14,10 +14,11 @@ public final class ConnectionConfig {
     public static Connection getConnection() throws SQLException {
         var url = "jdbc:mysql://localhost/board";
         var user = "board";
-        var password = "board";
+        var password = "senha123"; // Mantendo a senha local
         var connection = DriverManager.getConnection(url, user, password);
         connection.setAutoCommit(false);
         return connection;
     }
 
 }
+
